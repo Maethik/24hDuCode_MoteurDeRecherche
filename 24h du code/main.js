ba84bf32edda9e451720e9a480f445ce4e77d2c6
KBM106 <96017088+kbm-106@users.noreply.github.com>
2023-05-05 01:26:46 +0200

    fini.....
    manques les lien

---
+++ 24h du code/main.js
@@ -0,0 +1,5 @@
+const icon = document.getElementById("random-article-icon");
+
+icon.addEventListener("click", function() {
+  window.location.href = "https://fr.wikipedia.org/wiki/Special:Random";
+});
